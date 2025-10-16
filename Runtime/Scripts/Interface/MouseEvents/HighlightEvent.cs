9fc560305cc36b4f8cf52be782296a81cf7012f1
liambarden <liamofbarden@gmail.com>
2025-10-16 15:19:47 +0100

    refactored highlight hierarchy

--- Runtime/Scripts/Interface/MouseEvents/HighlightEvent.cs
+++ Runtime/Scripts/Interface/MouseEvents/HighlightEvent.cs
@@ -1,110 +1,16 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-namespace LycheeLabs.FruityInterface {
+﻿namespace LycheeLabs.FruityInterface {
 
     public class HighlightEvent : InterfaceEvent {
 
+        private static readonly HighlightHierarchy hierarchy = new HighlightHierarchy();
+
         public HighlightParams Params;
 
-        private readonly struct HighlightLink {
-            public HighlightLink(MouseTarget target, InterfaceNode node) {
-                Target = target;
-                Node = node;
-            }
-
-            public MouseTarget Target { get; }
-            public InterfaceNode Node { get; }
-        }
-
-        private static readonly List<HighlightLink> currentAncestry = new List<HighlightLink>(8);
-        private static readonly List<HighlightLink> previousAncestry = new List<HighlightLink>(8);
-
         public void Activate(bool logging) {
-            BuildAncestry(currentAncestry);
-
-            var newTarget = Params.Target;
-            var sameTarget = newTarget == FruityUI.HighlightedTarget;
-            var branchFirstFrame = !sameTarget && newTarget != null;
-
-            DiffAndApply(newTarget, logging, sameTarget, branchFirstFrame);
-
-            previousAncestry.Clear();
-            previousAncestry.AddRange(currentAncestry);
+            hierarchy.Build(Params);
+            hierarchy.ApplyDiff(logging, Params);
         }
 
-        private void DiffAndApply(MouseTarget newTarget, bool logging, bool isSameTarget, bool branchFirstFrame) {
-            var sharedTailCount = FindSharedTailCount(currentAncestry, previousAncestry);
-            var previousUniqueCount = previousAncestry.Count - sharedTailCount;
-            var currentUniqueCount = currentAncestry.Count - sharedTailCount;
-
-            for (var i = 0; i < previousUniqueCount; i++) {
-                previousAncestry[i].Target?.MouseHoverEnd();
-            }
-
-            if (!isSameTarget) {
-                FruityUI.HighlightedTarget = newTarget;
-            }
-
-            ApplyHoverSegment(currentAncestry, 0, currentUniqueCount, branchFirstFrame);
-            ApplyHoverSegment(currentAncestry, currentUniqueCount, currentAncestry.Count, firstFrame: false);
-
-            if (logging && newTarget != null && !isSameTarget) {
-                Debug.Log("Highlight: " + newTarget);
-            }
-        }
-
-        private void BuildAncestry(List<HighlightLink> ancestry) {
-            ancestry.Clear();
-
-            var target = Params.Target;
-            var node = Params.Node;
-            if (target != null) {
-                ancestry.Add(new HighlightLink(target, node));
-            }
-
-            var parentNode = node != null ? node.InputParent : null;
-            while (parentNode != null) {
-                var parentTarget = parentNode.GetMouseTarget(Params.MouseWorldPosition, Params.HeldButton);
-                if (parentTarget != null) {
-                    ancestry.Add(new HighlightLink(parentTarget, parentNode));
-                }
-                parentNode = parentNode.InputParent;
-            }
-        }
-
-        private void ApplyHoverSegment(List<HighlightLink> ancestry, int startIndexInclusive, int endIndexExclusive, bool firstFrame) {
-            if (ancestry == null) {
-                return;
-            }
-
-            var clampedEnd = Mathf.Min(endIndexExclusive, ancestry.Count);
-            for (var i = startIndexInclusive; i < clampedEnd; i++) {
-                var link = ancestry[i];
-                var target = link.Target;
-                if (target == null) {
-                    continue;
-                }
-
-                var parameters = ReferenceEquals(target, Params.Target) ? Params : HighlightParams.blank;
-                target.MouseHovering(firstFrame, parameters);
-            }
-        }
-
-        private static int FindSharedTailCount(List<HighlightLink> current, List<HighlightLink> previous) {
-            var shared = 0;
-            while (shared < current.Count && shared < previous.Count) {
-                var currentLink = current[current.Count - 1 - shared];
-                var previousLink = previous[previous.Count - 1 - shared];
-
-                if (!ReferenceEquals(currentLink.Target, previousLink.Target) ||
-                    !ReferenceEquals(currentLink.Node, previousLink.Node)) {
-                    break;
-                }
-                shared++;
-            }
-            return shared;
-        }
     }
 
 }
---
+++ Runtime/Scripts/Interface/MouseEvents/HighlightHierarchy.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace LycheeLabs.FruityInterface {
+    public class HighlightHierarchy {
+
+        private readonly List<HighlightLink> current = new List<HighlightLink>(8);
+        private readonly List<HighlightLink> previous = new List<HighlightLink>(8);
+
+        public MouseTarget CurrentTarget { get; private set; }
+
+        public void Build(HighlightParams @params) {
+            current.Clear();
+
+            var target = @params.Target;
+            var node = @params.Node;
+            CurrentTarget = target;
+
+            if (target != null) {
+                current.Add(new HighlightLink(target, node));
+            }
+
+            var parentNode = node != null ? node.InputParent : null;
+            while (parentNode != null) {
+                var parentTarget = parentNode.GetMouseTarget(@params.MouseWorldPosition, @params.HeldButton);
+                if (parentTarget != null) {
+                    current.Add(new HighlightLink(parentTarget, parentNode));
+                }
+                parentNode = parentNode.InputParent;
+            }
+        }
+
+        public void ApplyDiff(bool logging, HighlightParams @params) {
+            var newTarget = CurrentTarget;
+            var sameTarget = newTarget == FruityUI.HighlightedTarget;
+            var branchFirstFrame = !sameTarget && newTarget != null;
+
+            var sharedTailCount = FindSharedTailCount(current, previous);
+            var previousUniqueCount = previous.Count - sharedTailCount;
+            var currentUniqueCount = current.Count - sharedTailCount;
+
+            for (var i = 0; i < previousUniqueCount; i++) {
+                previous[i].Target?.MouseHoverEnd();
+            }
+
+            if (!sameTarget) {
+                FruityUI.HighlightedTarget = newTarget;
+            }
+
+            ApplyHoverSegment(current, 0, currentUniqueCount, @params, branchFirstFrame);
+            ApplyHoverSegment(current, currentUniqueCount, current.Count, @params, false);
+
+            if (logging && newTarget != null && !sameTarget) {
+                Debug.Log("Highlight: " + newTarget);
+            }
+
+            previous.Clear();
+            previous.AddRange(current);
+        }
+
+        private static void ApplyHoverSegment(List<HighlightLink> ancestry, int startIndexInclusive, int endIndexExclusive, HighlightParams @params, bool firstFrame) {
+            if (ancestry == null) {
+                return;
+            }
+
+            var clampedEnd = Mathf.Min(endIndexExclusive, ancestry.Count);
+            for (var i = startIndexInclusive; i < clampedEnd; i++) {
+                var link = ancestry[i];
+                var target = link.Target;
+                if (target == null) {
+                    continue;
+                }
+
+                var parameters = ReferenceEquals(target, @params.Target) ? @params : HighlightParams.blank;
+                target.MouseHovering(firstFrame, parameters);
+            }
+        }
+
+        private static int FindSharedTailCount(List<HighlightLink> current, List<HighlightLink> previous) {
+            var shared = 0;
+            while (shared < current.Count && shared < previous.Count) {
+                var currentLink = current[current.Count - 1 - shared];
+                var previousLink = previous[previous.Count - 1 - shared];
+
+                if (!ReferenceEquals(currentLink.Target, previousLink.Target) ||
+                    !ReferenceEquals(currentLink.Node, previousLink.Node)) {
+                    break;
+                }
+                shared++;
+            }
+            return shared;
+        }
+
+        private readonly struct HighlightLink {
+            public HighlightLink(MouseTarget target, InterfaceNode node) {
+                Target = target;
+                Node = node;
+            }
+
+            public MouseTarget Target { get; }
+            public InterfaceNode Node { get; }
+        }
+    }
+
+}
